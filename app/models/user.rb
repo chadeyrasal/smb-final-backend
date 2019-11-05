0c07237c9f4ec6593e2ebf39d54ea2fc8d074c2f
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-05 16:29:23 +0000

    add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_many :trips, through: :bicycles
   has_many :reservations, class_name: "Trip", foreign_key: :renter_id
 
+  validates :fullname, :email, presence: true
+  validates :email, uniqueness: true
+
   has_secure_password
 
 end
