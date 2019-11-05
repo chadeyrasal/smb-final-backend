2d919eb643326d5e55e2d51411d56c3dce29ddd8
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-05 17:44:20 +0000

    add index, create, show and destroy controller actions & city_params and set_city private methods to CitiesController

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::CitiesController < ApplicationController
+
+  before_action :set_city, only: [:show, :destroy]
+
+  def index
+    @cities = City.all
+    render json: @cities
+  end
+
+  def create
+    @city = City.new(city_params)
+    if @city.save
+      render json: @city
+    else
+      render json: { error: "There was an error creating this city" }
+    end
+  end
+
+  def show
+    render json: @city
+  end
+
+  def destroy
+    @city.destroy
+  end
+
+
+  private
+
+  def city_params
+    params.require(:city).permit(:name, :country_id)
+  end
+
+  def set_city
+    @city = City.find(params[:id])
+  end
+
 end
