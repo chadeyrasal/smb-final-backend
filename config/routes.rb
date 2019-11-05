cc2fd98817bf39c75d6b2f2cf32efc03b0619cb3
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-05 13:47:07 +0000

    add namespace to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :trips
-  resources :neighbourhoods
-  resources :countries
-  resources :cities
-  resources :bicycles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :trips
+      resources :neighbourhoods
+      resources :countries
+      resources :cities
+      resources :bicycles
+    end
+  end
+
 end
