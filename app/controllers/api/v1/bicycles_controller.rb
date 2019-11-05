2b2cc96395f7555b8912565ceeed2a37c31529a9
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-05 17:40:03 +0000

    add index, create, show and destroy controller actions & bicycle_params and set_bicycle private methods to BicyclesController

--- app/controllers/api/v1/bicycles_controller.rb
+++ app/controllers/api/v1/bicycles_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::BicyclesController < ApplicationController
+
+  before_action :set_bicycle, only: [:show, :destroy]
+
+  def index
+    @bicycles = Bicycle.all
+    render json: @bicycles
+  end
+
+  def create
+    @bicycle = Account.new(bicycle_params)
+    if @bicycle.save
+      render json: @bicycle
+    else
+      render json: { error: "There was an error creating this bicycle" }
+    end
+  end
+
+  def show
+    render json: @bicycle
+  end
+
+  def destroy
+    @bicycle.destroy
+  end
+
+
+  private
+
+  def bicycle_params
+    params.require(:bicycle).permit(:title, :description, :bicycle_type, :size, :colour, :price, :neighbourhood_id, :city_id, :country_id)
+  end
+
+  def set_bicycle
+    @bicycle = Bicycle.find(params[:id])
+  end
+
 end
