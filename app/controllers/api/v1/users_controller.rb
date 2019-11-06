2e421af9b89119ea364881ab49f9852278f0242d
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-06 19:02:41 +0000

    add index, create, show and destroy controller actions & user_params and set_user private methods to UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :destroy]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { error: "There was an error creating this user" }
+    end
+  end
+
+  def show
+    render json: @user
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:fullname, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
