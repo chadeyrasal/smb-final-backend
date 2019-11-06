d737bde3b6fbde04c95870e99fd575312b3486b8
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-06 18:59:57 +0000

    add index, create, show and destroy controller actions & trip_params and set_trip private methods to TripsController

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::TripsController < ApplicationController
+
+  before_action :set_trip, only: [:show, :destroy]
+
+  def index
+    @trips = Trip.all
+    render json: @trips
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      render json: @trip
+    else
+      render json: { error: "There was an error creating this trip" }
+    end
+  end
+
+  def show
+    render json: @trip
+  end
+
+  def destroy
+    @trip.destroy
+  end
+
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:start_date, :end_date, :rating, :review, :bicycle_id, :renter_id)
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
 end
