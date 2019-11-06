90e48af4188489f9fd2a61b136bbd3819eccd72a
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-06 14:12:10 +0000

    add index, create, show and destroy controller actions & country_params and set_country private methods to CountriesController

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::CountriesController < ApplicationController
+
+  before_action :set_country, only: [:show, :destroy]
+
+  def index
+    @countries = Country.all
+    render json: @countries
+  end
+
+  def create
+    @country = Country.new(country_params)
+    if @country.save
+      render json: @country
+    else
+      render json: { error: "There was an error creating this country" }
+    end
+  end
+
+  def show
+    render json: @country
+  end
+
+  def destroy
+    @country.destroy
+  end
+
+
+  private
+
+  def country_params
+    params.require(:country).permit(:name)
+  end
+
+  def set_country
+    @country = Country.find(params[:id])
+  end
+
+  end
+
 end
