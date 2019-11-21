a947a1cbd13388a7c30fa1aecec5201cf7f843ae
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-21 17:45:51 +0000

    add nested bicycle index routes under neighbourhood, city and user

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show, :create]
+      resources :users, only: [:show, :create] do
+        resources :bicycles, only: [:index]
+      end
       resources :trips, only: [:index, :show, :create, :update]
-      resources :neighbourhoods, only: [:index, :show, :create]
+      resources :neighbourhoods, only: [:index, :show, :create] do
+        resources :bicycles, only: [:index]
+      end
       resources :countries, only: [:create]
-      resources :cities, only: [:index, :show, :create]
-      resources :bicycles, only: [:index, :show, :create, :update]
+      resources :cities, only: [:index, :show, :create] do
+        resources :bicycles, only: [:index]
+      end
+      resources :bicycles, only: [:show, :create, :update]
     end
   end
 
