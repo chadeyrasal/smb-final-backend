632dda7def1c1a41a39fbeec90284131781acca3
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-21 17:46:44 +0000

    move neighbourhood index route under cities

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
         resources :bicycles, only: [:index]
       end
       resources :trips, only: [:index, :show, :create, :update]
-      resources :neighbourhoods, only: [:index, :show, :create] do
+      resources :neighbourhoods, only: [:show, :create] do
         resources :bicycles, only: [:index]
       end
       resources :countries, only: [:create]
       resources :cities, only: [:index, :show, :create] do
         resources :bicycles, only: [:index]
+        resources :neighbourhoods, only: [:index]
       end
       resources :bicycles, only: [:show, :create, :update]
     end
