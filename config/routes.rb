3d0bb7a5695c570a4461d4a7d0c2648f7073f54e
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-11-21 17:20:02 +0000

    add update route for trips to allow review and rating

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:show, :create]
-      resources :trips, only: [:index, :show, :create]
+      resources :trips, only: [:index, :show, :create, :update]
       resources :neighbourhoods, only: [:index, :show, :create]
       resources :countries, only: [:create]
       resources :cities, only: [:index, :show, :create]
