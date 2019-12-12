abccc9f5fd2fb0eb68d6aaf646026690c099197b
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-12 20:05:56 +0000

    change login route to POST request instead of GET

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
         resources :neighbourhoods, only: [:index]
       end
       resources :bicycles, only: [:show, :create, :update]
+      post '/login', to: 'sessions#create'
     end
   end
 
