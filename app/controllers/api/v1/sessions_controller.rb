570712c80bf459edea394fbcf04dc5fefacfbd3a
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-12 20:09:37 +0000

    create sessions controller that allows a new session to be created when a user inputs correct credentials, makes the current user persist and allows logging out by destroying the current session

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: {
+        error: 'Invalid credentials'
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: 'no one logged in'
+      }
+    end
+  end
+
+  def destroy
+    session.clear
+  end
+
+end
