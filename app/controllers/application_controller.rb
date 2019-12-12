f211c941d34746c9c68cd5ae455adafdd094b36e
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-12 14:49:59 +0000

    remove helpers do

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::API
 
-  helpers do
-    def current_user
-      User.first
-    end
+  def current_user
+    User.first
+  end
 
-    def logged_in?
-      !!current_user
-    end
+  def logged_in?
+    !!current_user
   end
 
 end
