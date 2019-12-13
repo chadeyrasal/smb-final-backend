9029e9b615607b6181c32602f8f8e3d48e32f392
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-13 15:44:26 +0000

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       resources :bicycles, only: [:show, :create, :update]
       post '/login', to: 'sessions#create'
       get '/get_current_user', to: 'sessions#get_current_user'
+      delete '/logout', to: 'sessions#destroy'
     end
   end
 
