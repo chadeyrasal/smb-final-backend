ac2ff63f79542aac0c8cff34fb016ce7c38c16b6
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-30 13:32:38 +0000

    build bicycle serializer

--- app/serializers/bicycle_serializer.rb
+++ app/serializers/bicycle_serializer.rb
@@ -1,4 +1,10 @@
 class BicycleSerializer < ActiveModel::Serializer
-  attributes :id
-  belongs_to :user
+
+  attributes :id, :bicycle_type, :size, :colour, :title, :description, :price, :neighbourhood, :city, :country, :owner
+
+  belongs_to :owner, class_name: 'User'
+  belongs_to :neighbourhood
+  has_many :trips
+  has_many :renters, class_name: 'User', through: :trips
+
 end
