a05cf4ea755f9282af2b6845f84e31637e8c5ebe
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-31 17:04:11 +0000

    create post signup route

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
       end
       resources :bicycles, only: [:index, :show, :create, :update]
       post '/login', to: 'sessions#create'
+      post '/signup', to: 'users#create'
       get '/get_current_user', to: 'sessions#get_current_user'
       delete '/logout', to: 'sessions#destroy'
     end
