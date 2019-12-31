24bce569a586a04ab375276ba3ba9318af20b7bc
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-12-31 17:05:54 +0000

    modify create method to log the user in after it is created in the data base

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,9 +3,11 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       render json: user
     else
-      render json: { error: "There was an error creating this user" }
+      response = { user.errors.full_messages.to_sentence }
+      render json: response,status: :unprocessable_entity
     end
   end
 
