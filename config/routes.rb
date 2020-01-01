ba7e49f1f16aac88428726660094fa8ceaa074f1
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-01 18:03:38 +0000

    add user index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show, :create] do
+      resources :users, only: [:index, :show, :create] do
         resources :bicycles, only: [:index]
       end
       resources :trips, only: [:index, :show, :create, :update]
