3662bd7725eb770a754c72c355ecb5122a8af41f
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-01 18:05:14 +0000

    add index action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UsersController < ApplicationController
 
+  def index
+    users= User.all
+    render json: users
+  end
+
   def create
     user = User.new(user_params)
     if user.save
