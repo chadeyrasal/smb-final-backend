57a9f2f4061b26015da609a3ca82a8e211423fd2
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-09 18:15:41 +0000

    add email to attributes of user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :fullname, :description, :created_at
+  attributes :id, :fullname, :description, :created_at, :email
   has_many :bicycles
   has_many :trips
 end
