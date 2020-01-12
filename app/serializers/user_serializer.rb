3092cfea279b7429fc4b790a30419316500f1117
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-12 19:34:24 +0000

    modify relationships

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :fullname, :description, :created_at, :email
-  has_many :bicycles
-  has_many :trips
+  has_many :bicycles, foreign_key: :owner_id
+  has_many :reservations, class_name: "Trip", foreign_key: :renter_id
 end
