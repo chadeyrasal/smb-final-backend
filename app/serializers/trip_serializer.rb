9a7426084cd549c797f2b5209a994a0e46396ef9
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-12 19:26:27 +0000

    add attributes and relationships to bicycle and renter in serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,3 +1,5 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :start_date, :end_date, :review, :rating
+  belongs_to :bicycle
+  belongs_to :renter, class_name: "User"
 end
