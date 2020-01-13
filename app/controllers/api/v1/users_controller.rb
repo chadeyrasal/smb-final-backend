35219f4e92bf144f2b94c9aa91e76ada9fc3c894
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-13 13:23:26 +0000

    add description as a permitted parameter

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:fullname, :email, :password)
+    params.require(:user).permit(:fullname, :email, :password, :description)
   end
 
 end
