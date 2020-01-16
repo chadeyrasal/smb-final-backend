1c460553ee96946a9425cf18ecebf1fdc80ffa24
chadeyrasal <annelaure.chadeyras@gmail.com>
2020-01-16 16:49:02 +0100

    list bicycle and renter as trip attributes

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,5 +1,5 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :start_date, :end_date, :review, :rating
+  attributes :id, :start_date, :end_date, :review, :rating, :bicycle, :renter 
   belongs_to :bicycle
   belongs_to :renter, class_name: "User"
 end
